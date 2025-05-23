6f030ff318048570a372803d143ab276dd8ec570
hab927 <148649543+csigua@users.noreply.github.com>
2025-05-23 10:18:47 -0700

    first commit

---
+++ src/Scenes/Platformer.js
@@ -0,0 +1,179 @@
+class Platformer extends Phaser.Scene {
+    constructor() {
+        super("platformerScene");
+    }
+
+    init() {
+        // variables and settings
+        this.ACCELERATION = 1000;
+        this.DRAG = 20000;    // DRAG < ACCELERATION = icy slide
+        this.AIRDRAG = 4000;
+        this.physics.world.gravity.y = 3000;
+        this.JUMP_VELOCITY = -800;
+        this.PARTICLE_VELOCITY = 50;
+        this.SCALE = 2.0;
+    }
+
+    create() {
+        // Create a new tilemap game object which uses 18x18 pixel tiles, and is
+        // 45 tiles wide and 25 tiles tall.
+        this.map = this.add.tilemap("platformer-level-1", 16, 16, 144, 27);
+
+        console.log(this.map)
+
+        // Add a tileset to the map
+        // First parameter: name we gave the tileset in Tiled
+        // Second parameter: key for the tilesheet (from this.load.image in Load.js)
+        this.tileset = this.map.addTilesetImage("monochrome_tilemap_packed", "tilemap_tiles");
+
+        // Create a layer
+        this.groundLayer = this.map.createLayer("Ground-n-Platforms", this.tileset, 0, 0);
+
+        // Make it collidable
+        this.groundLayer.setCollisionByProperty({
+            collides: true
+        });
+
+        // TODO: Add createFromObjects here
+        // Find coins in the "Objects" layer in Phaser
+        // Look for them by finding objects with the name "coin"
+        // Assign the coin texture from the tilemap_sheet sprite sheet
+        // Phaser docs:
+        // https://newdocs.phaser.io/docs/3.80.0/focus/Phaser.Tilemaps.Tilemap-createFromObjects
+
+        this.coins = this.map.createFromObjects("Objects", {
+            name: "coin",
+            key: "tilemap_sheet",
+            frame: 21
+        });
+
+        this.spawn = this.map.createFromObjects("Objects", {
+            name: "spawn",
+            key: "tilemap_sheet",
+            frame: 0
+        });
+
+        this.physics.world.TILE_BIAS = 20;
+
+        // TODO: Add turn into Arcade Physics here
+        // Since createFromObjects returns an array of regular Sprites, we need to convert 
+        // them into Arcade Physics sprites (STATIC_BODY, so they don't move) 
+        this.physics.world.enable(this.coins, Phaser.Physics.Arcade.STATIC_BODY);
+
+        // Create a Phaser group out of the array this.coins
+        // This will be used for collision detection below.
+        this.coinGroup = this.add.group(this.coins);
+
+        // set up player avatar
+        my.sprite.player = this.physics.add.sprite(this.spawn[0].x, this.spawn[0].y, "character_sheet");
+        my.sprite.player.setCollideWorldBounds(true);
+
+        // Enable collision handling
+        this.physics.add.collider(my.sprite.player, this.groundLayer);
+
+        // TODO: Add coin collision handler
+        // Handle collision detection with coins
+        this.physics.add.overlap(my.sprite.player, this.coinGroup, (obj1, obj2) => {
+            obj2.destroy(); // remove coin on overlap
+        });
+
+        // set up Phaser-provided cursor key input
+        cursors = this.input.keyboard.createCursorKeys();
+
+        this.rKey = this.input.keyboard.addKey('R');
+
+        // debug key listener (assigned to D key)
+        this.input.keyboard.on('keydown-D', () => {
+            this.physics.world.drawDebug = this.physics.world.drawDebug ? false : true
+            this.physics.world.debugGraphic.clear()
+        }, this);
+
+        // TODO: Add movement vfx here
+
+        my.vfx.walking = this.add.particles(0, 6, "kenny-particles", {
+            frame: ['trace_03.png', 'trace_02.png'],
+            random: true,
+            scale: {start: 0.03, end: 0.02},
+            lifespan: 350,
+            gravityY: 1200,
+            alpha: {start: 1, end: 0.1}
+        });
+
+        my.vfx.jump = this.add.particles(0, 6, "kenny-particles", {
+            frame: ['trace_05.png'],
+            scale: {start: 0.06, end: 0.01},
+            duration: 50,
+            lifespan: 400,
+            gravityY: 1000,
+            alpha: {start: 1, end: 0},
+            maxAliveParticles: 10,
+        })
+
+        my.vfx.walking.stop();
+        my.vfx.jump.stop();
+
+        // TODO: add camera code here
+        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+        this.cameras.main.startFollow(my.sprite.player, true, 0.25, 0.25); // (target, [,roundPixels][,lerpX][,lerpY])
+        this.cameras.main.setDeadzone(50, 50);
+        this.cameras.main.setZoom(this.SCALE);
+    }
+
+    update() {
+        if(cursors.left.isDown) {
+            my.sprite.player.setAccelerationX(-this.ACCELERATION);
+            my.sprite.player.setFlip(true, false);
+            my.sprite.player.anims.play('walk', true);
+
+            // TODO: add particle following code here
+            my.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth/2-10, my.sprite.player.displayHeight/2-5, false);
+            my.vfx.walking.setParticleSpeed(this.PARTICLE_VELOCITY, 0);
+            // Only play smoke effect if touching the ground
+            if (my.sprite.player.body.blocked.down) {
+                my.vfx.walking.start();
+            }
+            else {
+                my.vfx.walking.stop();
+            }
+
+        } else if(cursors.right.isDown) {
+            my.sprite.player.setAccelerationX(this.ACCELERATION);
+            my.sprite.player.resetFlip();
+            my.sprite.player.anims.play('walk', true);
+            // TODO: add particle following code here
+            my.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth/2-10, my.sprite.player.displayHeight/2-5, false);
+            my.vfx.walking.setParticleSpeed(this.PARTICLE_VELOCITY, 0);
+            // Only play smoke effect if touching the ground
+            if (my.sprite.player.body.blocked.down) {
+                my.vfx.walking.start();
+            }
+            else {
+                my.vfx.walking.stop();
+            }
+
+        } else {
+            // Set acceleration to 0 and have DRAG take over
+            my.sprite.player.setAccelerationX(0);
+            my.sprite.player.setDragX(this.DRAG);
+            my.sprite.player.anims.play('idle');
+            // TODO: have the vfx stop playing
+            my.vfx.walking.stop();
+        }
+
+        // player jump
+        // note that we need body.blocked rather than body.touching b/c the former applies to tilemap tiles and the latter to the "ground"
+        if(!my.sprite.player.body.blocked.down) {
+            my.sprite.player.anims.play('jump');
+        }
+        if(my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+            my.sprite.player.body.setVelocityY(this.JUMP_VELOCITY);
+            my.vfx.jump.x = my.sprite.player.body.x + my.sprite.player.body.displayWidth/2;
+            my.vfx.jump.y = my.sprite.player.body.y + my.sprite.player.body.displayHeight;
+            my.vfx.jump.start();
+        }
+
+        if(Phaser.Input.Keyboard.JustDown(this.rKey)) {
+            this.scene.restart();
+        }
+    }
+}
